a8bf4d727a4a69429bef549696d1647e4b77c3e0
jxxtty <jaehee941106@gmail.com>
2022-05-24 15:14:36 +0900

    항목 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {useState} from "react";
+import {useState, useRef, useCallback} from "react";
 import './App.css';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
@@ -24,9 +24,25 @@ const App = () => {
         }
     ]);
 
+    // useState가 아닌 useRef로 관리하는 이유 -> id값은 렌더링되는 정보가 아니기 때문(화면에 보이지도 않고, 이 값이 변한다해서 컴포넌트가 리렌더링될 필요가 없음)
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false
+            }
+            setTodos(todos.concat(todo));
+            nextId.current += 1;
+        },
+        [todos]
+    );
+
     return (
         <TodoTemplate>
-            <TodoInsert/>
+            <TodoInsert onInsert={onInsert}/>
             <TodoList todos={todos}/> {/* TodoList에서 'todos'를 props로 받아 처리하게 한다. */}
         </TodoTemplate>
     )
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,27 @@
 import './TodoInsert.scss'
 import {MdAdd} from "react-icons/md";
+import {useState, useCallback} from "react"; // useState, useCallback import
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(event => { // 함수를 한번 만들고 재사용할 수 있도록 useCallback 사용
+        setValue(event.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        event => {
+            onInsert(value);
+            setValue('');
+
+            event.preventDefault(); // submit 이벤트는 브라우저에서 새로고침을 유발한다 -> event.preventDefault를 사용하면 이를 방지해준다.
+        },
+        [onInsert, value]
+    );
+
     return (
-        <form className={"TodoInsert"}>
-            <input placeholder={"할 일을 입력하세요"} />
+        <form className={"TodoInsert"} onSubmit={onSubmit}>
+            <input placeholder={"할 일을 입력하세요"} value={value} onChange={onChange} />
             <button type={"submit"}>
                 <MdAdd/>
             </button>
